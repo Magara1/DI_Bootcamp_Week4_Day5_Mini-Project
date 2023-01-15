ae01bf9f38c6695066a631d84d2bb0643846098f
Magara Etchien N'gouan <magaraetchien@live.fr>
2023-01-15 19:10:34 +0000

    Week4 Day5 Mini-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
-  providers: [],
+  providers: [BooksComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ 
+ }
